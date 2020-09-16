38644d76746048929a9c5a40a9f7e13e5925f400
Jorge Dias <jorgediasdsg@gmail.com>
2020-09-16 20:22:12 -0300

    🚀 chore: First Class

---
+++ index.js
@@ -0,0 +1,4 @@
+//   Class Make bot Discord
+//   https://youtu.be/KYnXhtyqQRQ
+//   https://discord.com/developers/applications/
+//   Add bot discordapp.com/oauth2/authorize?=&client_id=###&scope=bot&permissions=8
