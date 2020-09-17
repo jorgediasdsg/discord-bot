f7779b5a3afd84839d22b57f920cc7b05e37497e
Jorge Dias <jorgediasdsg@gmail.com>
2020-09-16 21:47:09 -0300

    🚀 chore: Ocult Discord Key

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 //   https://youtu.be/KYnXhtyqQRQ
 //   https://discord.com/developers/applications/
 //   Add bot discordapp.com/oauth2/authorize?=&client_id=###&scope=bot&permissions=8
+
+const Discord = require("discord.js");
+const client = new Discord.client();
+const config = require("./config.json")
+
+client.on("ready", () => {
+    console.log(`Bot foi iniciado com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores`)
+    client.user.setGame(`Eu estou em ${client.guilds.size} servidores`)
+})
